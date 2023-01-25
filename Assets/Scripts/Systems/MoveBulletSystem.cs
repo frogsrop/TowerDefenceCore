c5c8d873b43ca301b60bd399c87c1a242fcf1f92
iSweetJS <67283947+SweetJS64@users.noreply.github.com>
2023-01-25 19:07:57 +0400

    add IJobEntity

--- Assets/Scripts/Systems/MoveBulletSystem.cs
+++ Assets/Scripts/Systems/MoveBulletSystem.cs
@@ -1,79 +1,86 @@
-// using System.Collections.Generic;
-// using Unity.Collections;
-// using Unity.Entities;
-// using Unity.Mathematics;
-// using Unity.Transforms;
-//
-// public partial class MoveBulletSystem : SystemBase
-// {
-//     int IndexOf(NativeArray<EnemyIdComponent> array, int id)
-//     {
-//         for (int i = 0; i < array.Length; i++)
-//         {
-//             if (array[i].Id == id)
-//             {
-//                 return i;
-//             }
-//         }
-//
-//         return -1;
-//     }
-//
-//     private static Dictionary<int, AbstractEffectConfig> _mapping;
-//
-//     protected override void OnStartRunning()
-//     {
-//         _mapping = AbstractEffectConfig.Mapping;
-//     }
-//
-//     protected override void OnUpdate()
-//     {
-//         var ecb = new EntityCommandBuffer(Allocator.TempJob);
-//         var queryEnemy = GetEntityQuery(ComponentType.ReadOnly<EnemyIdComponent>(),
-//             ComponentType.ReadOnly<LocalToWorldTransform>(),
-//             ComponentType.ReadOnly<DamageBufferElement>(),
-//             ComponentType.ReadOnly<BurningBufferElement>());
-//         var dt = SystemAPI.Time.DeltaTime;
-//
-//         var enemyIds = queryEnemy.ToComponentDataArray<EnemyIdComponent>(Allocator.Temp);
-//         var enemyTransforms = queryEnemy.ToComponentDataArray<LocalToWorldTransform>(Allocator.Temp);
-//         var enemyEntityArray = queryEnemy.ToEntityArray(Allocator.Temp);
-//         if (enemyIds.Length > 0)
-//         {
-//             Entities.WithAll<TargetIdComponent>().ForEach(
-//                 (ref LocalToWorldTransform bulletTransform, in TargetIdComponent bullet, in BulletComponent bulletInfo,
-//                     in Entity entity) =>
-//                 {
-//                     var enemyIndex = IndexOf(enemyIds, bullet.Id);
-//
-//                     if (enemyIndex != -1)
-//                     {
-//                         var enemyTransform = enemyTransforms[enemyIndex];
-//                         var enemyEntity = enemyEntityArray[enemyIndex];
-//                         var direction = math.normalize(enemyTransform.Value.Position - bulletTransform.Value.Position);
-//                         bulletTransform.Value.Position += direction * dt * 10;
-//                         var distance = math.distancesq(bulletTransform.Value.Position, enemyTransform.Value.Position);
-//                         if (distance < 0.1f)
-//                         {
-//                             ecb.DestroyEntity(entity);
-//
-//                             foreach (var effect in bulletInfo.ListEffects)
-//                             {
-//                                 if (_mapping.ContainsKey(effect))
-//                                 {
-//                                     _mapping[effect].AppendToBuffer(enemyEntity, ecb);
-//                                 }
-//                             }
-//                         }
-//                     }
-//                     else
-//                     {
-//                         ecb.DestroyEntity(entity);
-//                     }
-//                 }).WithoutBurst().Run();
-//             Dependency.Complete();
-//             ecb.Playback(EntityManager);
-//             ecb.Dispose();
-//         }
-//     }
-// }
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+
+public partial class MoveBulletSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var targetIdQuery = GetEntityQuery(ComponentType.ReadOnly<TargetIdComponent>());
+         var queryEnemy = GetEntityQuery(ComponentType.ReadOnly<EnemyIdComponent>(),
+             ComponentType.ReadOnly<LocalToWorldTransform>(),
+             ComponentType.ReadOnly<DamageBufferElement>(),
+             ComponentType.ReadOnly<BurningBufferElement>());
+         var enemyIds = queryEnemy.ToComponentDataArray<EnemyIdComponent>(Allocator.TempJob);
+         var ecb = new EntityCommandBuffer(Allocator.TempJob);
+         if (enemyIds.Length > 0)
+         {
+             var dt = SystemAPI.Time.DeltaTime;
+             new MoveBulletJob
+             {
+                 dtJob = dt,
+                 ecbJob = ecb,
+                 enemyIdsInJob = enemyIds,
+                 enemyTransformsInJob = queryEnemy.ToComponentDataArray<LocalToWorldTransform>(Allocator.TempJob),
+                 enemyEntityArrayInJob = queryEnemy.ToEntityArray(Allocator.TempJob)
+             }.Run(targetIdQuery);
+             Dependency.Complete();
+             ecb.Playback(EntityManager);
+             ecb.Dispose();
+        }
+    }
+}
+
+public partial struct MoveBulletJob : IJobEntity
+{
+    public float dtJob;
+    public EntityCommandBuffer ecbJob;
+    public NativeArray<EnemyIdComponent> enemyIdsInJob;
+    public NativeArray<LocalToWorldTransform> enemyTransformsInJob;
+    public NativeArray<Entity> enemyEntityArrayInJob;
+    
+    public void Execute(ref LocalToWorldTransform bulletTransform, in TargetIdComponent bullet, in BulletComponent bulletInfo,
+        in Entity entity)
+    {
+        var mapping = AbstractEffectConfig.Mapping;
+        var enemyIndex = IndexOf(enemyIdsInJob, bullet.Id);
+        if (enemyIndex != -1)
+        {
+            var enemyTransform = enemyTransformsInJob[enemyIndex];
+            var enemyEntity = enemyEntityArrayInJob[enemyIndex];
+            var direction = math.normalize(enemyTransform.Value.Position - bulletTransform.Value.Position);
+            bulletTransform.Value.Position += direction * dtJob * 10;
+            var distance = math.distancesq(bulletTransform.Value.Position, enemyTransform.Value.Position);
+            if (distance < 0.1f)
+            {
+                ecbJob.DestroyEntity(entity);
+                foreach (var effect in bulletInfo.ListEffects)
+                {
+                    if (mapping.ContainsKey(effect))
+                    {
+                        mapping[effect].AppendToBuffer(enemyEntity, ecbJob);
+                    }
+                }
+            }
+        }
+        else
+        {
+            ecbJob.DestroyEntity(entity);
+        }
+    }
+    
+    int IndexOf(NativeArray<EnemyIdComponent> array, int id)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Id == id)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
